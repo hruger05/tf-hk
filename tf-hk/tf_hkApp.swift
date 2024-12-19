994255974423fced2c316308a252a12a81f338a8
Hudson Ruger <rugerhudson@icloud.com>
2024-12-19 10:19:19 -0500

    Initial Commit

---
+++ tf-hk/tf_hkApp.swift
@@ -0,0 +1,17 @@
+//
+//  tf_hkApp.swift
+//  tf-hk
+//
+//  Created by Hudson Ruger on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct tf_hkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
